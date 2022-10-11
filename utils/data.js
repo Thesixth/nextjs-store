453bccacad3e51be05a0aa5d0f8c80807c955d62
palinendir@gmail.com <palinendir@gmail.com>
2022-10-10 19:35:55 -0700

    first commit

---
+++ utils/data.js
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      name: "Smart Reractable Wallet",
+      slug: "retractable-wallet",
+      category: "accessories",
+      image: "/images/wallet.png",
+      price: 70,
+      brand: "pali",
+      rating: 4.8,
+      numReviews: 8,
+      countInStock: 20,
+      description: "Access you cards with a touch of a clip",
+    },
+    {
+      name: "Retro Wallet",
+      slug: "retro-wallet",
+      category: "accessories",
+      image: "/images/wallet.png",
+      price: 70,
+      brand: "pali",
+      rating: 4.8,
+      numReviews: 8,
+      countInStock: 20,
+      description: "Access you cards with a touch of a clip",
+    },
+  ],
+};
+
+export default data;
